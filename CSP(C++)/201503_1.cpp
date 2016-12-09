c362983613ed866adc20152ac0b0440adf96c1eb
cfj <2218250273@qq.com>
2016-12-09 08:46:22 +0800

    Notes:comments

--- CSP(C++)/201503_1.cpp
+++ CSP(C++)/201503_1.cpp
@@ -1,32 +1,20 @@
-#include <iostream>
-#include <vector>
+#include<iostream>
 using namespace std;
+
+int a[1005][1005];	//这么大的数组一定要定义为全局变量，如果放在main函数里面会莫名其妙报错
 
 int main()
 {
-	int n=0;
-	int m=0;
-	cin >> n;
-	cin	>> m;
-
-	vector<int> nm;
-	int temp = 0;
-	for(int ci=0;ci < n*m ;ci++)
-	{
-		cin >> temp;
-		nm.push_back(temp);
-	}
-
-	for(int max=m-1;max>=0;max--)
-	{
-		for(int num = 0; num<nm.size(); num++)
-		{
-			if(num%m == max)
-				cout << nm[num] << " ";
-		}
-		cout << endl;
-	}
-	
-
-	return 0;
+    int n,m;
+    cin >> n >> m;
+    for(int i = 0;i <= n-1;i++)
+    {
+        for(int j = 0;j <= m-1;j++)   cin >> a[i][j];
+    }
+    for(int j = m-1;j >= 0;j--)
+    {
+        for(int i = 0;i <= n-1;i++)   cout << a[i][j] << ' ';
+        cout << endl;
+    }
+    return 0;
 }
