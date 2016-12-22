f8ca47eb63a1d810d906eeba1065a58719c39207
cfj <2218250273@qq.com>
2016-12-22 22:35:31 +0800

    Though passed.But not all tests pass.

--- CSP(C++)/201503_3.cpp
+++ CSP(C++)/201503_3.cpp
@@ -1,40 +1,33 @@
 #include <iostream>
 using namespace std;
 
-bool isRun(int year)
+int ping_year[12] = {31,28,31,30,31,30,31,31,30,31,30,31};		
+int run_year[12]  = {31,29,31,30,31,30,31,31,30,31,30,31};
+int a,b,c;
+int y_1,y_2;
+
+bool isRun(int year)			
 {
 	return (year%400==0 || (year%4==0 && year%100!=0));
 }
 
-void output(int a)
+void output(int z)
 {
-	if(a<10)
-		cout << "0" << a;
+	if(z<10)
+		cout << "0" << z;
 	else
-		cout << a;
+		cout << z;
 }
 
-void handleEachyear()
+void handleEachyear(int year)
 {
-
-}
-
-int main()
-{
-	int a,b,c;
-	cin >> a >> b >> c;
-	int y1,y2;
-	cin >> y1 >> y2;
-	int ping_year[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
-	int run_year[12]  = {31,29,31,30,31,30,31,31,30,31,30,31};
-
-	int day_1_1 = 0;
+	int day_1_1;
 	int temp = 0;
-	if(y1 == 1850)		//y1年1月1日是 周day_1_1
+	if(year == 1850)		//year年1月1日是 周day_1_1
 		day_1_1 = 2;
 	else
 	{
-		for(int i=1850;i<y1;i++)
+		for(int i=1850;i<year;i++)
 		{
 			if(isRun(i))
 				temp += 366;
@@ -47,14 +40,14 @@ int main()
 		temp = 0;
 	}
 
-	int day_a_1 = 0;
-	if(a == 1)			//y1年a月1日是 周day_a_1
+	int day_a_1;
+	if(a == 1)			//year年a月1日是 周day_a_1
 		day_a_1 = day_1_1;
 	else
 	{
 		for(int i=1;i<a;i++)
 		{
-			if(isRun(y1))
+			if(isRun(year))
 				temp += run_year[i-1];
 			else
 				temp += ping_year[i-1];
@@ -67,7 +60,7 @@ int main()
 
 	if(b == 1)
 	{
-		cout << y1 << "/" ;
+		cout << year << "/" ;
 		output(a);
 		cout << "/" ;
 		if(c==day_a_1)
@@ -75,42 +68,47 @@ int main()
 		else if(c > day_a_1)
 			output(c- day_a_1 + 1);
 		else
-			output(day_a_1 + 8 - c);
+			output(c+ 8 -day_a_1);
 		cout << endl;
 	}
 	else
 	{
-		if(b>5)
-			cout << "none" << endl;
-		else if ((c == day_a_1 && 1+(b-1)*7 <= ping_year[a-1] && !isRun(y1)) || (c == day_a_1 && 1+(b-1)*7 <= run_year[a-1] && isRun(y1)))
+		if ((c == day_a_1 && 1+(b-1)*7 <= ping_year[a-1] && !isRun(year)) || (c == day_a_1 && 1+(b-1)*7 <= run_year[a-1] && isRun(year)))
 		{
-			cout << y1 << "/" ;
+			cout << year << "/" ;
 			output(a);
 			cout << "/" ;
 			output(1+(b-1)*7);
 			cout << endl;
 		}
-		else if ((c > day_a_1 && c- day_a_1 + 1+(b-1)*7 <= ping_year[a-1] && !isRun(y1)) || (c > day_a_1 && c- day_a_1 + 1+(b-1)*7 <= run_year[a-1] && isRun(y1)))
+		else if ((c > day_a_1 && c- day_a_1 + 1+(b-1)*7 <= ping_year[a-1] && !isRun(year)) || (c > day_a_1 && c- day_a_1 + 1+(b-1)*7 <= run_year[a-1] && isRun(year)))
 		{
-			cout << y1 << "/" ;
+			cout << year << "/" ;
 			output(a);
 			cout << "/" ;
 			output( c- day_a_1 + 1+(b-1)*7);
 			cout << endl;
 		}
-		else if ((c < day_a_1 &&day_a_1 + 8 - c+(b-1)*7 <= ping_year[a-1] && !isRun(y1)) || (c < day_a_1 && day_a_1 + 8 - c+(b-1)*7 <= run_year[a-1] && isRun(y1)))
+		else if ((c < day_a_1 &&c+ 8 -day_a_1+(b-1)*7 <= ping_year[a-1] && !isRun(year)) || (c < day_a_1 && c+ 8 -day_a_1+(b-1)*7 <= run_year[a-1] && isRun(year)))
 		{
-			cout << y1 << "/" ;
+			cout << year << "/" ;
 			output(a);
 			cout << "/" ;
-			output( day_a_1 + 8 - c+(b-1)*7);
+			output( c+ 8 -day_a_1+(b-1)*7);
 			cout << endl;
 		}
 		else
 			cout << "none" << endl;
 	}
+}
 
+int main()
+{
+	cin >> a >> b >> c;
+	cin >> y_1 >> y_2;
 	
+	for(int j=y_1;j<=y_2;j++)
+		handleEachyear(j);
 
 	return 0;
 }
